8a2585eddec10a4ab29c11a03404ac615f052627
arsalanahmed12 <khanarsal884@gmail.com>
2024-11-20 23:38:05 +0500

    milestone 1 static interactive resume builder

---
+++ script.js
@@ -0,0 +1,10 @@
+var toggleButton = document.getElementById('toggle');
+var skill = document.getElementById('skill');
+toggleButton.addEventListener('click', function () {
+    if (skill.style.display === 'none') {
+        skill.style.display = 'block';
+    }
+    else {
+        skill.style.display = 'none';
+    }
+});
---
+++ script.ts
@@ -0,0 +1,9 @@
+const toggleButton = document.getElementById("toggle") as HTMLButtonElement;
+const skill = document.getElementById("skill") as HTMLElement;
+toggleButton.addEventListener("click", () => {
+  if (skill.style.display === "none") {
+    skill.style.display = "block";
+  } else {
+    skill.style.display = "none";
+  }
+});
